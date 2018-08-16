8e162c48145daead3ceb6da914d8791ebcdd7e64
xiongluglinm <xiong.lu.glinm@gmail.com>
2018-08-16 23:16:11 +0800

    本次更新：实践JSONP入门，用script标签跨网站发请求。

--- main.js
+++ main.js
@@ -1,22 +1,10 @@
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
-// var port = process.argv[2]
 var port = process.env.PORT || 8888;
 
-// if(!port){
-//   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-//   process.exit(1)
-// }
 
 var server = http.createServer(function(request, response){
-  // var parsedUrl = url.parse(request.url, true)
-  // var pathWithQuery = request.url 
-  // var queryString = ''
-  // if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-  // var path = parsedUrl.pathname
-  // var query = parsedUrl.query
-  // var method = request.method
 
   var temp = url.parse(request.url,true)
   var path = temp.pathname
@@ -24,18 +12,6 @@ var server = http.createServer(function(request, response){
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
-
-
-
-
-
-
-
-
-
-
-
-
 
 
   console.log('HTTP 路径为\n' + path)
@@ -59,20 +35,12 @@ var server = http.createServer(function(request, response){
     }else if(path === '/pay'){
       var amount = fs.readFileSync('./db','utf8')
       var newAmount = amount - 1
-      // if(Math.random() > 0.5){
       fs.writeFileSync('./db', newAmount)
       response.setHeader('Content-Type','application/javascript')
       response.statusCode = 200
       response.write(`
-      // alert("付款成功")
-      // window.location.reload()
-      // amount.innerText = amount.innerText - 1
         ${query.callback}.call(undefined,'success')
       `)
-      // }else{
-      //   response.statusCode = 400
-      //   response.write('fail')
-      // }
       response.end()
     }else{
       response.statusCode = 404
@@ -80,15 +48,9 @@ var server = http.createServer(function(request, response){
       response.write('找不到对应的路径')
       response.end()
     }
-
-
-
-
-
+    
   /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
